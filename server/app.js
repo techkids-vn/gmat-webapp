c282780ddae9b9783f1fda1dd733edb8fd641532
thekeldeo <khanhktong@gmail.com>
2016-06-20 14:33:20 +0700

    .

--- server/app.js
+++ server/app.js
@@ -20,6 +20,7 @@ app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
   next();
 });
+//a
 // Connect mongo
 mongoose.connect(config.mongoURI);
 var db = mongoose.connection;
